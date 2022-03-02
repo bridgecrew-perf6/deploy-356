6f162cd250111892ff9bbbe648e19e79e6490029
mipbr2000 <mipbr2000@yahoo.com.br>
2022-03-01 23:07:08 -0300

    Update JavaJsonHandler.java

--- JavaJsonHandler.java
+++ JavaJsonHandler.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.FileWriter;
 
-public class Main {
+public class JavaJsonHandler {
     private static FileWriter fileToWrite;
 
     public static void main(String[] args) throws Exception {
