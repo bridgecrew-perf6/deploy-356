ccdd0a79b0724ebe7679c9f038d3b4ec467128ed
mipbr2000 <mipbr2000@yahoo.com.br>
2022-03-01 23:04:15 -0300

    Update JavaJsonHandler.java

--- JavaJsonHandler.java
+++ JavaJsonHandler.java
@@ -4,8 +4,10 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.FileWriter;
 
-public class JavaJsonHandler {
+public class Main {
+    private static FileWriter fileToWrite;
 
     public static void main(String[] args) throws Exception {
         String filename = "./json/DeploymentPlan.json";
@@ -14,9 +16,19 @@ public class JavaJsonHandler {
             JSONObject o = new JSONObject(contents);
 
             JSONObject deploy = new JSONObject(o.getJSONObject("Deploy").toString());
+
             System.out.println(deploy.getString("PackageName"));
+            deploy.put("PackageName", "testando");
+            deploy.append("teste", "teste");
+            System.out.println(deploy.getString("PackageName"));
+
             System.out.println(deploy.getString("Name"));
             System.out.println(deploy.getString("_comment"));
+
+            fileToWrite = new FileWriter("./json/DeploymentPlanNew.json");
+            fileToWrite.write(deploy.toString());
+            fileToWrite.flush();
+            fileToWrite.close();
 
             JSONArray task = o.getJSONArray("Task");
             for (int i = 0; i < task.length(); i++) {
